1ea91704a696964a6eb5ff3a8e8190e11f8d1743
datky <tkachenko.d@cdek.ru>
2024-07-23 20:44:19 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(4, 5);
+
+        System.out.println(result);
+
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void test() {
+        Calculator calculator = new Calculator();
+        int first = 5;
+        int second = 5;
+
+
+        int result = calculator.sum(first, second);
+
+
+    }
+
+
+}
